21abd12b9eb682c4361ec1af8510452fab7bba60
Nero-Leffen <ipsit.ex0309@gmail.com>
2022-12-03 13:17:10 +0530

    TASK 5 Completed

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
-    if connection == None:
+    if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
@@ -16,7 +16,7 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit == True:
+    if commit is True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
@@ -35,4 +35,7 @@ def close_connection(exception):
 @app.route('/jobs')
 @app.route('/')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
+                       'employer.name as '
+                       'employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
